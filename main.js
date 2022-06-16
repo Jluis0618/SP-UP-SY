f26d433b487efc6c23233ae6da4d5d3933a3cd36
Jluis0618 <85001208+Jluis0618@users.noreply.github.com>
2022-06-16 07:10:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.izquierda h2', {
+    duration: 1000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  sr.reveal('.izquierda .linea', {
+    duration: 1000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  sr.reveal('.izquierda p', {
+    duration: 1000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.manual h3', {
+    duration: 1000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  sr.reveal('.manual a', {
+    duration: 1000,
+    origin: 'right',
+    distance: '-100px'
+  });
+
+  sr.reveal('.manual img', {
+    duration: 1000,
+    origin: 'right',
+    distance: '-100px'
+  });
+
+  sr.reveal('.contact h2', {
+    duration: 1000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+  sr.reveal('.contact a', {
+    duration: 1000,
+    origin: 'left',
+    distance: '-100px'
+  });
